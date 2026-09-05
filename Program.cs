1360b459187fdc090a3a2a18ead1aad4fc8a70bb
stully18 <stullyrl@gmail.com>
2026-09-05 15:38:47 -0500

    Building out Rider Parent Class

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Rider test = new Rider("Male", 31,4);
+
+for (int i = 0; i < 1000; i++)
+{
+    test.doTrick();
+}
--- models/rider.cs
+++ models/rider.cs
@@ -1,17 +1,52 @@
-abstract class Rider
-{
-    public string gender = "";
-    public int age;
-    public int yearsOfExperience;
-    protected int health = 100;
-
-    public Rider(string gender, int age, int experience)
+     class Rider
     {
-        this.gender = gender;
-        this.age = age;
-        yearsOfExperience = experience;
+        public string gender = "";
+        public int age;
+        public int yearsOfExperience;
+        protected int health = 79;
+        private int trickDamage = 1;
+        private double trickModifier = 1.1;
+
+        public Rider(string gender, int age, int experience)
+        {
+            this.gender = gender;
+            this.age = age;
+            yearsOfExperience = experience;
+        }
+
+        public virtual void doTrick()
+        {
+            if (willLand(health) == true)
+            {
+                System.Console.WriteLine("You landed the trick!");
+            }
+            else
+            {
+                health -= trickDamage;
+                string message = $"You failed!  You lost {trickDamage} health :()";
+                System.Console.WriteLine(message);
+            }
+        }
+
+        public bool willLand(int health)
+        {
+            double chances = (double) health * trickModifier ;
+            Random rand = new Random();
+
+            int randomNum = rand.Next(1,101);
+
+            if ((double)randomNum <= chances)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+            
+        }
+
+
+
     }
-
-
-
-}
