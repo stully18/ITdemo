9101ee48f7a8790412a98477de51d005b9e026e8
stully18 <stullyrl@gmail.com>
2026-09-09 10:52:39 -0400

    Built out constructor input

--- Program.cs
+++ Program.cs
@@ -1,8 +1,26 @@
-﻿Snowboarder shane = new Snowboarder("Goofy", "Male", 31,4);
+﻿//Snowboarder shane = new Snowboarder("Goofy", "Male", 31,4);
 
-shane.GetHealth();
+/*shane.GetHealth();
 shane.Eat();
 shane.GetHealth();
 shane.GetStance();
 shane.SwitchStance();
-shane.GetStance();
+shane.GetStance();*/
+
+System.Console.WriteLine("Welcome to Shane's Resort! Create your Rider!");
+System.Console.Write("Choose your sport! 1. Skier 2. Snowboarder (1 or 2): ");
+
+int sport = int.Parse(Console.ReadLine());
+
+if (sport == 1)
+{
+    System.Console.WriteLine("You chose Sking!");
+}
+else if (sport == 2)
+{
+    System.Console.WriteLine("You chose Snowboarding!");
+}
+else
+{
+    System.Console.WriteLine("Please input valid number!");
+}
