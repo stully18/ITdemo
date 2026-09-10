1badb85b0c33046590ea8e2274d73accfe71cb90
stully18 <stullyrl@gmail.com>
2026-09-10 13:26:37 -0400

    Added questions for Snowboarder Class

--- models/snowboarder.cs
+++ models/snowboarder.cs
@@ -34,4 +34,43 @@ class Snowboarder : Rider
         }
     }
 
+    public static Snowboarder CreateFromInput()
+    {
+        string stance = "Goofy";
+        int age;
+        int experience;
+        string gender;
+        string response = "";
+        
+        System.Console.WriteLine("Lets Build your snowboarder!");
+        System.Console.Write("First choose your stance: (Goofy or Regular)");
+        response = Console.ReadLine().ToLower();
+        if (response == "g")
+        {
+            stance = "Goofy";
+        }
+        else if (response == "r")
+        {
+            stance = "Regular";
+        }
+        else
+        {
+            System.Console.WriteLine($"{response} is not Valid. Please select correct stance!");
+        }
+
+        System.Console.Write("Enter age:  ");
+        response = Console.ReadLine();
+        age = int.Parse(response);
+
+        System.Console.Write("Enter years of Experience:  ");
+        response = Console.ReadLine();
+        experience = int.Parse(response);
+
+        System.Console.Write("Enter Gender:  ");
+        response = Console.ReadLine();
+        gender = response;
+
+        return new Snowboarder(stance, gender, age, experience);
+    }
+
 } 
